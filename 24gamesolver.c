d046e4a09c0ad5a567bc54e1ffc2732e540482e7
Kalvin Kepler <qilin2603379191@gmail.com>
2025-10-28 23:17:46 +0100

    up

--- 24gamesolver.c
+++ 24gamesolver.c
@@ -1,21 +1,8 @@
-/* file    : 24gamesolver.c */
-/* author  : XueJin Cui */
-/* date    : 2025/10/24 */
-/* version : 2.0 */
-/* Description:
- * The program ask for 4 numbers between 1 and 10 from users, and prints the
- * expresssion that equals 24 if possible, otherwise it notifies user that
- * there is no solution.
- * The main idea is to use recursion to try all possible operations between
- * all pairs of numbers until only one number is left, then check if it is 24.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 void input(float numbers[],char expr[][100]) {
-   // Input function to read 4 numbers between 1 and 10
    float a;
    printf("Input 4 numbers between 1 and 10 (inclusive):\n");
    for (int i=0;i<4;i++) {
@@ -29,14 +16,12 @@ void input(float numbers[],char expr[][100]) {
       }
    }
 
-   //initialize expressions
    for (int j=0;j<4;j++) {
       snprintf(expr[j],100,"%.0f",numbers[j]);
    }
 }
 
 int isEqual(float a,float b) {
-   // check equality of two floats
    return (fabsf(a-b)<1e-6);
 }
 
@@ -44,13 +29,11 @@ int isPossible(
    int length, float numbers[4],
    char expressions[4][100], char result[100]) {
 
-      float newNumbers[4]; // to hold new numbers
-      char newExpressions[4][100]; // to hold new expressions
+      float newNumbers[4]; 
+      char newExpressions[4][100];
 
-      // if only one number left, check if it is 24
       if (length == 1) {
          if (isEqual(numbers[0],24)) {
-            //set result
             snprintf(result,100,"%s",expressions[0]);
             return 1;
          } else {
@@ -58,11 +41,9 @@ int isPossible(
          }
       }
 
-      // use double loops to try all pairs
       for (int i=0;i<length;i++) {
          for (int j=i+1;j<length;j++) {
 
-            //store remaining numbers and expressions first
             int index = 0;
             for (int k=0;k<length;k++) {
                if ((k!=i)&&(k!=j)) {
@@ -72,24 +53,15 @@ int isPossible(
                } 
             } 
 
-            //Then we try all operations, for addition and multiplication, we
-            //only need to do one order, for subtraction and division, we need 
-            //to try both orders
-
-            //addition:
-            //calculate the new number and store the new expression
             newNumbers[index] = numbers[i] + numbers[j]; 
             snprintf (
                newExpressions[index],100,
                "(%s + %s)",expressions[i],expressions[j]);
 
-            // use the new number and expression to do recursion
             if (isPossible(length-1,newNumbers,newExpressions,result)) {
                return 1;
             }
 
-            //other operations follow the same pattern
-            //multiplication:
             newNumbers[index] = numbers[i] * numbers[j];
             snprintf (
                newExpressions[index],100,
@@ -99,8 +71,6 @@ int isPossible(
                return 1;
             }
             
-
-            //subtraction (order 1):
             newNumbers[index] = numbers[i] - numbers[j];
             snprintf (
                newExpressions[index],100,
@@ -110,7 +80,6 @@ int isPossible(
                return 1;
             }
 
-            //subtraction (order 2):
             newNumbers[index] = numbers[j] - numbers[i];
             snprintf (
                newExpressions[index],100,
@@ -120,8 +89,6 @@ int isPossible(
                return 1;
             }
 
-            //division (order 1):
-            //for division, we need to make sure the denominator is not zero
             if (!isEqual(numbers[i],0)) {
                newNumbers[index] = numbers[j] / numbers[i];
                snprintf (
@@ -133,7 +100,6 @@ int isPossible(
                }
             }
 
-            //division (order 2):
             if (!isEqual(numbers[j],0)) {
                newNumbers[index] = numbers[i] / numbers[j];
                snprintf (
@@ -158,7 +124,7 @@ int main(int argc, char *argv[]) {
    if (isPossible(4,numbers,expressions,result)) {
       printf("Solution found: %s = 24\n",result);
    } else {
-      printf ("No solution founded.\n");
+      printf ("No solution found.\n");
    }
    return 0;
 }
